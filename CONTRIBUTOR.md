bcacf3b5356eb99d95a717e735a6c13912f6f97e
Erik Tank <erik.tank@pluto.tv>
2023-10-30 07:42:22 -0700

    URL encode characters
    - add encoding of special characters
    - add CONTRIBUTOR.md

---
+++ CONTRIBUTOR.md
@@ -0,0 +1,20 @@
+# Joplin Alfred Workflow Contributor Guidelines
+
+## Requirements
+
+- Curl
+- Perl 5
+
+### Jundy::AlfredWorkflow
+
+[This repository](https://github.com/skeletonkey/Jundy_AlfredWorkflow) contains all reusable code. You will also want to fork it.
+
+## Contribute
+
+- create (or claim) an [issue](https://github.com/skeletonkey/joplin_alfred_workflow/issues)
+  - assign it to yourself
+  - all communication will be done through Github Issues
+- submit pull request
+  - a code owner should review and provide feedback within a week of submittal/response
+  - the PR author is required to response to all feedback within a month of it being provided
+  - PRs not responded to within 2 months will be removed
--- Joplin.pm
+++ Joplin.pm
@@ -2,18 +2,23 @@ package Joplin;
 
 use JSON;
 
-# https://joplinapp.org/api/references/rest_api/
+use Jundy::AlfredWorkflow::HTTP;
+
+# https://joplinapp.org/help/api/references/rest_api#using-the-api
 
 sub get_data {
     my $url_parts = shift || [];
     my $query_map = shift || {};
 
     $query_map->{token} = $ENV{JOPLIN_TOKEN};
+    $query_map->{fields} = 'id,title';
+    $query_map->{limit} = 9;
+    $query_map->{order_by} = "updated_time";
 
     my $url = join("/", $ENV{JOPLIN_URL}, @$url_parts);
     if ($query_map) {
         $url .= '?';
-        $url .= join("&", map { "$_=$query_map->{$_}"} keys %$query_map);
+        $url .= join("&", map { "$_=" . Jundy::AlfredWorkflow::HTTP::url_encode($query_map->{$_})} keys %$query_map);
     }
 
     my $resp = `curl -s -q "$url"`;
