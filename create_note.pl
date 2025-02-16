870bca13ba98142a352a8730062f903041339e8f
Erik Tank <shiny.gift6738@fastmail.com>
2025-02-15 20:13:38 -0700

    jn allow for creating a note title (#12)
    ask for a note title during creation
    Update README.md

--- create_note.pl
+++ create_note.pl
@@ -6,12 +6,13 @@ use warnings;
 use Joplin;
 
 my $folder_id = $ARGV[0];
+my $title     = $ARGV[1];
 
 my $ret = Joplin::post_data(
     ['notes'],
     {},
     {
-        title     => '',
+        title     => $title,
         body      => '',
         parent_id => $folder_id,
     }
